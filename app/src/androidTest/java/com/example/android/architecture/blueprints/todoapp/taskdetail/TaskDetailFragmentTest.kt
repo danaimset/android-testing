d9cdb8ab17ace6d059331a89fd5c50ed1ee9a576
Daniil Lukashevich <danaimset@gmail.com>
2020-06-28 20:22:28 +0300

    8. Task: Launch a Fragment from a Test - Step 2. Make a TaskDetailFragmentTest class

---
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/taskdetail/TaskDetailFragmentTest.kt
@@ -0,0 +1,11 @@
+package com.example.android.architecture.blueprints.todoapp.taskdetail
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.MediumTest
+import org.junit.runner.RunWith
+
+@MediumTest
+@RunWith(AndroidJUnit4::class)
+class TaskDetailFragmentTest {
+
+}
